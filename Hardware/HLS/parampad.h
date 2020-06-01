3a805fa9d74a9693263f50cac0e24f2d20e8caf9
ahmedyahia1060 <44532481+ahmedyahia1060@users.noreply.github.com>
2020-06-01 23:49:13 +0200

    Delete parampad.h

--- Hardware/HLS/parampad.h
+++
@@ -1,7 +0,0 @@
-#define matrix_dim_input 3
-#define matrix_dim_paded 5
-
-void matrixpadding(
-	int matrix_input[matrix_dim_input][matrix_dim_input],
-	int matrix_paded[matrix_dim_paded][matrix_dim_paded]
-);	
