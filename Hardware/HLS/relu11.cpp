e9be4e8f442b4a7bf971143358b69826e3b4276f
ahmedyahia1060 <44532481+ahmedyahia1060@users.noreply.github.com>
2020-06-09 00:32:49 +0200

    Add files via upload

---
+++ Hardware/HLS/relu11.cpp
@@ -0,0 +1,60 @@
+//============================================================================
+// Name        : relu11.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#define COLUMN_SIZE 4
+#define Size_Row 4
+#define SIZE_3D 4
+
+int array[Size_Row][COLUMN_SIZE]={0};
+void relu_array(const int a[] [COLUMN_SIZE], int rowSize)
+{
+
+
+
+    for (int row = 0; row < rowSize; row++)
+    {
+        for (int column = 0; column < COLUMN_SIZE; column++)
+        {
+            if(a[row][column]>=0)
+            	{array[row][column]=a[row][column];
+            	}
+        }
+    }
+
+
+}
+int main()
+{
+    int m[COLUMN_SIZE][Size_Row];
+    for(int i=0;i<COLUMN_SIZE;i++)
+           {
+           	for(int j=0;j<Size_Row;j++)
+           	{cin>>m[i][j];
+           	}
+           }
+
+    int var=0;
+    if(var==SIZE_3D)
+    {
+    relu_array(m,Size_Row);
+    var++;
+    }
+    for(int i=0;i<COLUMN_SIZE;i++)
+           {
+           	for(int j=0;j<Size_Row;j++)
+           	{
+           		cout <<array[i][j];
+           		cout <<"     ";
+           	}
+           }
+
+    return 0;
+}
