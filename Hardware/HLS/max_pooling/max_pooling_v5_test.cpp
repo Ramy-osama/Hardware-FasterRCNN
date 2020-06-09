f6b71f6c322c3b0c993978c5f618e4abda0157c2
ahmedyahia1060 <44532481+ahmedyahia1060@users.noreply.github.com>
2020-06-09 04:02:38 +0200

    Add files via upload

---
+++ Hardware/HLS/max_pooling/max_pooling_v5_test.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<stdio.h>
+#include<vector>
+#include <stdlib.h> 
+#include<time.h> 
+#define r 6
+#define c 6
+#define d 3
+using namespace std;
+void Max_Pooling(int image[r][c][d],int stride);
+int main() {
+int stride=2;
+int image [r][c][d];
+  srand(time(0));
+	for (int k=0;k<=2;k=k+1)
+	{
+		for(int i=0;i<=r-1;i++)
+		{
+			for(int j=0;j<=c-1;j++)
+			{
+				image[i][j][k]=rand()%100;
+			}
+		}
+	}
+	
+	for(int k=0;k<=2;k++)
+{
+	cout<<endl;
+cout<<"channel "<<k+1<< ".\n";
+cout<<endl;
+for(int i=0;i<=r-1;i++)// print the output
+{
+	cout<<"\n";
+	for(int j=0;j<=c-1;j++)
+	{
+		cout<<image[i][j][k]<<" ";
+		
+	}
+}
+cout<<endl;
+}
+  Max_Pooling(image,stride);
+return 0;
+}
